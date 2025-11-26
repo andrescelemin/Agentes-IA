b20376607804502e6eee2ddba6e29ed7227bbc88
Andres Celemin <andrescelemincardoso@gmail.com>
2025-11-26 10:39:11 -0500

    Eliminar carpeta duplicada

--- Smart Prompt Pro (2)/src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Utility functions for class name management and common operations
- */
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
